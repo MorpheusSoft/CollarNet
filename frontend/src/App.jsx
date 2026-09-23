39b180dc62b06ad9d548dd43d2e903fb2ded0cb3
Lindbergh Zambrano <lindberghzambrano@gmail.com>
2026-09-23 12:10:25 -0400

    fix(frontend): import useRef in App.jsx to fix blank black screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import LandingPage from './views/LandingPage';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
